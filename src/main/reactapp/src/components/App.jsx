dbc61f05a3aae68197cc3caba6875e530d8d9735
ithodol <ddmdjesz1@gmail.com>
2025-03-20 12:04:58 +0900

    Spring+React WEB2 퍼시스턴스 적용

--- src/main/reactapp/src/components/App.jsx
+++ src/main/reactapp/src/components/App.jsx
@@ -6,24 +6,30 @@ import Header from './layouts/Header'
 import Footer from './layouts/Footer'
 import Signup from './member/Signup'
 import Login from './member/Login'
-import store from './reduxs/store'
-import {Provider} from 'react-redux'
+
+// import할 때 export default 적용된 자료는 { } 생략 가능
+// 여러개를 가져오거나 default가 아닌 자료는 { } 사용해야함
+import { store, persistor } from './reduxs/store'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 export default function App(Props){
     return(<>
         <Provider store = {store}> {/* 리덕스 스토어 적용 : 현재 Provider로 감싼 컴포넌트 모두 리덕스 전역상태 사용 가능 */}
-            <BrowserRouter> {/* 모든 라우팅을 감싼다 */}
-                <div id='wrap'>
-                    <Header/> {/* Routes 밖에 있으면 고정 */}
-                    <Routes> {/* Routes 안에 있으면 전환 */}
-                        <Route path='/' element={<Home/>}></Route>
-                        <Route path='/member/signup' element={<Signup/>}></Route>
-                        <Route path='/member/login' element={<Login/>}></Route>
-                    </Routes>
-                    <Footer/> {/* Routes 밖에 있으면 고정 */}
-                </div>
-            </BrowserRouter>
+            <PersistGate persistor={persistor} loading={null}>{/* 퍼시스턴스 적용할 컴포넌트를 모두 감싼다 */}
+                <BrowserRouter> {/* 모든 라우팅을 감싼다 */}
+                    <div id='wrap'>
+                        <Header/> {/* Routes 밖에 있으면 고정 */}
+                        <Routes> {/* Routes 안에 있으면 전환 */}
+                            <Route path='/' element={<Home/>}></Route>
+                            <Route path='/member/signup' element={<Signup/>}></Route>
+                            <Route path='/member/login' element={<Login/>}></Route>
+                        </Routes>
+                        <Footer/> {/* Routes 밖에 있으면 고정 */}
+                    </div>
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </>)
 }
--- src/main/reactapp/src/components/reduxs/store.jsx
+++ src/main/reactapp/src/components/reduxs/store.jsx
@@ -6,8 +6,31 @@ import userReducer from './userSlice'
 // (2) 변수에 configureStore 함수를 대입한다
 // (3) configureStore 함수의 매개변수에 리듀서 정의/등록
 // (4) { reducer : { 리듀서명1 : 등록할리듀서1, 리듀서명2 : 등록할리듀서2 } }
+
+
+// (6) 리덕스 퍼시스턴스 함수 가져오기
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // 브라우저의 localStorage 사용
+
+// (7) 퍼시스턴스 설정 / {storage, key : "key정의"}
+const persistConfig = {
+    storage, // localStorage에 사용 설정
+    key : "root" // localStorage에 저장할 키 설정    
+}
+
+// (8) 퍼시스턴스 적용할 리듀서 설정 / persistReducer(퍼시스턴스 설정, 적용할리듀서)
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
+
+
 export const store = configureStore({
-    reducer : {user : userReducer}
+    // reducer : {user : userReducer} // 퍼시스턴스 적용하기 전 리듀서
+    reducer : {user : persistedReducer} // 퍼시스턴스 적용된 리듀서
 })
+
+// (10) 퍼시스턴스가 적용된 스토어 내보내기
+export const persistor = persistStore(store);
+
+
 // (5) export로 store 내보내기
 export default store;
